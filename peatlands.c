8dd1bda6d08e1678b8a7ac53f5cc6c86e9e97f99
resinbeard <mrafoster@gmail.com>
2019-08-08 17:44:37 -0700

    Removing references to audio-related processing libs/headers

--- peatlands.c
+++ peatlands.c
@@ -1,5 +1,5 @@
-/* test_osc.c
-This file is a part of 'cyperus'
+/* peatlands.c
+This file is a part of 'peatlands'
 This program is free software: you can redistribute it and/or modify
 hit under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
@@ -12,23 +12,18 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-'cyperus' is a JACK client for learning about software synthesis
+'peatlands' is a monome frontend for cyperus.
+https://github.com/petrichorsystems/cyperus
 
-Copyright 2015 murray foster */
+Copyright 2019 murray foster */
 
 #include <stdio.h> //printf
 #include <string.h> //memset
 #include <stdlib.h> //exit(0);
-#include <jack/jack.h> 
 #include <math.h>
 #include <lo/lo.h>
-#include <fftw3.h>
 #include <monome.h>
 #include <signal.h>
-
-#include "test_osc.h"
-#include "rtqueue.h"
-#include "dsp.h"
 
 /* current state of monome grid */
 unsigned int grid[16][16] = { [0 ... 15][0 ... 15] = 0 };
@@ -40,21 +35,7 @@ char *monome_name_user_defined="init";
 char *monome_device_type;
 int monome_serialosc_port = 0;
 
-static jack_default_audio_sample_t ** outs ;
-static jack_default_audio_sample_t ** ins ;
-int samples_can_process = 0;
-
 lo_address lo_addr_send;
-
-rtqueue_t *fifo_in;
-rtqueue_t *fifo_out;
-
-jack_client_t *client=NULL;
-int jack_sr;
-const size_t sample_size = sizeof (jack_default_audio_sample_t) ;
-
-jack_port_t **output_port ;
-static jack_port_t **input_port ;
 
 /* MODULE MAX NO */
 #define MAXMODULES 4
@@ -320,42 +301,6 @@ monome_thread(void *arg)
   monome_event_loop(monome);
 } /* monome_thread */
 
-static int
-process(jack_nframes_t nframes, void *arg)
-{
-  float sample = 0; 
-  unsigned i, n, x; 
-  int sample_count = 0;
-
-  /* allocate all output buffers */
-  for(i = 0; i < 1; i++)
-    {
-      outs [i] = jack_port_get_buffer (output_port[i], nframes);
-      memset(outs[i], 0, nframes * sample_size);
-      ins [i] = jack_port_get_buffer (input_port[i], nframes);
-    }
-  
-  for ( i = 0; i < nframes; i++) {
-    if( !rtqueue_isfull(fifo_in) )
-      rtqueue_enq(fifo_in,ins[0][i]);
-    
-    if( !rtqueue_isempty(fifo_out) ){
-      outs[0][i] = rtqueue_deq(fifo_out);
-    }
-    else {
-      outs[0][i]=0;
-    } 
-  }
-  return 0 ;
-} /* process */
-
-static void
-jack_shutdown (void *arg)
-{
-  (void) arg ;
-  exit (1) ;
-} /* jack_shutdown */
-
 void
 update_modulation_state(int module_no, int param_no, int y)
 {
@@ -606,72 +551,6 @@ handle_lift(const monome_event_t *e, void *data)
   
 } /* handle_lift */
 
-void
-allocate_ports(int channels, int channels_in)
-{
-  int i = 0;
-  char name [16];
-  /* allocate output ports */
-  output_port = calloc (channels, sizeof (jack_port_t *)) ;
-  outs = calloc (channels, sizeof (jack_default_audio_sample_t *)) ;
-  for (i = 0 ; i < channels; i++)
-    {     
-      snprintf (name, sizeof (name), "out_%d", i + 1) ;
-      output_port [i] = jack_port_register (client, name, JACK_DEFAULT_AUDIO_TYPE, JackPortIsOutput, 0) ;
-    }
-  
-  /* allocate input ports */
-  size_t in_size = channels_in * sizeof (jack_default_audio_sample_t*);
-  input_port = (jack_port_t **) malloc (sizeof (jack_port_t *) * channels_in);
-  ins = (jack_default_audio_sample_t **) malloc (in_size);
-  memset(ins, 0, in_size);
-  
-  for( i = 0; i < channels_in; i++)
-    {      
-      snprintf( name, sizeof(name), "in_%d", i + 1);
-      input_port[i] = jack_port_register(client, name, JACK_DEFAULT_AUDIO_TYPE, JackPortIsInput, 0);
-    }
-} /* allocate_ports */
-
-int
-set_callbacks()
-{
-  /* Set up callbacks. */
-  jack_set_process_callback (client, process, NULL) ;
-  jack_on_shutdown (client, jack_shutdown, 0) ;
-  return 0;
-} /* set_callbacks */
-
-int
-jack_setup(char *client_name)
-{
-  /* create jack client */
-  if ((client = jack_client_open(client_name, JackNullOption,NULL)) == 0)
-    {
-      fprintf (stderr, "Jack server not running?\n") ;
-      return 1 ;
-    } ;
-
-  /* store jack server's samplerate */
-  jack_sr = jack_get_sample_rate (client) ;
-
-  fifo_out = rtqueue_init(jack_sr/256);
-  fifo_in = rtqueue_init(jack_sr/256);
-  
-  return 0;
-} /* jack_setup */
-
-int
-activate_client()
-{
-  /* Activate client. */
-  if (jack_activate (client))
-    {	
-      fprintf (stderr, "Cannot activate client.\n") ;
-      return 1 ;
-    }
-  return 0;
-} /* activate_client */
 
 void error(int num, const char *msg, const char *path)
 {
@@ -968,11 +847,6 @@ int osc_list_modules_handler(const char *path, const char *types, lo_arg ** argv
 int main(void)
 {
   struct monome_t *monome = NULL;
-  jack_setup("cyperus_osc_test");
-  set_callbacks();
-  if (activate_client() == 1)
-    return 1;
-  allocate_ports(1, 1);
 
   char *osc_port_in = "97217";
   char *osc_port_out = "97211";
@@ -1017,10 +891,6 @@ int main(void)
    and maintaining state */
   monome_register_handler(monome, MONOME_BUTTON_DOWN, handle_press, NULL);
   monome_register_handler(monome, MONOME_BUTTON_UP, handle_lift, NULL);
-
-  pthread_t dsp_thread_id;
-  pthread_create(&dsp_thread_id, NULL, dsp_thread, NULL);
-  pthread_detach(dsp_thread_id);
   
   pthread_t monome_thread_id;
   pthread_create(&monome_thread_id, NULL, monome_thread, monome);
